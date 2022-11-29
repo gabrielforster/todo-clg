04ea99fb0f343c1175d3b3322dbe099afe6f7e74
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-11-28 21:01:32 -0300

    feat: styling edit modal

--- src/components/EditModal/index.tsx
+++ src/components/EditModal/index.tsx
@@ -1,6 +1,8 @@
 import { XCircle } from 'phosphor-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from 'react-modal';
+
+import classes from './styles.module.scss';
 
 Modal.setAppElement('#root');
 
@@ -17,11 +19,16 @@ interface EditModalProps{
 
 export const EditModal = (props: EditModalProps) => {
 
-	const [taskName, setTaskName] = useState(props.task.name);
-	const [isCompleted, setIsCompleted] = useState(props.task.isCompleted);
+	const [taskName, setTaskName] = useState<string>();
+	const [isCompleted, setIsCompleted] = useState<boolean>();
+
+	useEffect(() => {
+		setTaskName(props.task.name);
+		setIsCompleted(props.task.isCompleted);
+	}, []);
 
 	function handleSave(){
-		props.onSave(props.task.id, taskName, isCompleted);
+		props.onSave(props.task.id, taskName as string, isCompleted as boolean);
 	}
 
 	return (
@@ -31,7 +38,7 @@ export const EditModal = (props: EditModalProps) => {
 			className="react-modal-content"
 			overlayClassName="react-modal-overlay"
 		>
-			<div className="modal-header">
+			<div className={classes.modalHeader}>
 				<h2>Editar Tarefa!</h2>
 
 				<button
@@ -42,24 +49,47 @@ export const EditModal = (props: EditModalProps) => {
 					<XCircle />
 				</button>
 			</div>
-			<div className="modal-body">
-				<input
-					type="text"
-					value={taskName}
-					onChange={(e) => setTaskName(e.target.value)}
-				/>
+			<div className={classes.modalBody}>
 
-				<div className="checkbox">
+				<div className={classes.inputGroup}>
+					<label htmlFor="taskName">Nome da Tarefa</label>
 					<input
-						type="checkbox"
-						checked={isCompleted}
-						onChange={(e) => setIsCompleted(e.target.checked)}
+						type="text"
+						name="taskName"
+						value={taskName}
+						onChange={(e) => setTaskName(e.target.value)}
 					/>
 				</div>
+
+				<div className={classes.checkbox}>
+					<label htmlFor="isCompleted">Tarefa concluída?</label>
+					<div
+						onClick={() => setIsCompleted(!isCompleted)}
+						className={classes.switch}
+					>
+						<input
+							name="isCompleted"
+							type="checkbox"
+							checked={isCompleted}
+						/>
+						<span className={[classes.slider, classes.round].join(' ')}></span>
+					</div>
+				</div>
 			</div>
-			<div className="modal-footer">
-				<button onClick={props.onClose}>Cancelar</button>
-				<button onClick={handleSave}>Salvar</button>
+			<div className={classes.modalFooter}>
+				<button
+					className={classes.cancel}
+					onClick={props.onClose}
+				>
+					Cancelar
+				</button>
+
+				<button
+					className={classes.save}
+					onClick={handleSave}
+				>
+					Salvar
+				</button>
 			</div>
 		</Modal>
 	);
